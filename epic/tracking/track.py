9368aa626ce10829e0c4e42f5a0ceeb2d4ee7024
Alphons <alphonsg@protonmail.com>
2021-07-20 19:38:17 +0800

    Redesign multiprocessing in track
    Old implementation was not compatible with GPU-based object detection, now have a single master process that does detection and spawned processes do rest.

--- epic/tracking/track.py
+++ epic/tracking/track.py
@@ -3,8 +3,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 import os
-from functools import partial
-from multiprocessing import Pool, Lock
+from multiprocessing import Process, Queue
 from shutil import rmtree
 
 from alive_progress import alive_bar
@@ -28,6 +27,8 @@ import yaml
 TRACKS_DIR_NAME = 'Tracks'
 MOTC_TRACKS_FILENAME = 'motc_tracks.txt'
 VID_FILENAME = 'video'
+
+SENTINEL = 'STOP'
 
 
 @click.command('tracking')
@@ -71,129 +72,178 @@ def track(root_dir, yaml_config, num_frames=None, anlys=False,
     with open(yaml_config) as f:
         config = yaml.safe_load(f)
 
-    if num_workers is None:
+    if num_workers is None:  # TODO len(os.sched_getaffinity(0))?
         num_workers = os.cpu_count() if os.cpu_count() is not None else 1
 
     if pre_proc:
         root_dir = preprocess.callback(root_dir, yaml_config, num_workers)
 
+    queue = Queue()
+
     epic.LOGGER.info(f'Processing root directory \'{root_dir}\'.')
-    tkr_fcty = TrackerFactory()
-    tracker = tkr_fcty.get_tracker(config['tracking']['tracker_name'], config)
     dirs = load_input_dirs(root_dir, multi_sequence)
     epic.LOGGER.info(f'Loaded {len(dirs)} image sequence(s).')
+    always = True if det == 'always' else False
+    i = 0
 
-    with alive_bar(len(dirs)) as main_bar:
-        if num_workers == 1:
-            for _ in (process(root_dir, yaml_config, tracker, num_frames,
-                      anlys, det, save_tracks, dets_min_score, vis_tracks,
-                      motchallenge, num_workers, curr_dir) for curr_dir in
-                      dirs):
+    if num_workers == 1:
+        with alive_bar(len(dirs)) as main_bar:
+            for input_dir in detect.callback(
+                    root_dir, yaml_config, vis_tracks, True,
+                    num_frames=num_frames, motchallenge=motchallenge,
+                    iterate=True, multi_sequence=True, always=always):
+                queue.put(input_dir)
+                queue.put(SENTINEL)
+                process(queue, root_dir, yaml_config, config,
+                        num_frames, anlys, save_tracks, dets_min_score,
+                        vis_tracks, motchallenge)
+                i += 1
                 main_bar()
-        else:
-            chunk_size = max(1, round(len(dirs) / num_workers))
-            lk = Lock()
-            with Pool(num_workers, initializer=init_lock, initargs=(lk,)) as p:
-                for _ in p.imap_unordered(partial(
-                        process, root_dir, yaml_config, tracker, num_frames,
-                        anlys, det, save_tracks, dets_min_score, vis_tracks,
-                        motchallenge, num_workers), dirs, chunk_size):
-                    main_bar()
+
+            main_bar(incr=len(dirs) - i)
+    else:
+        prog_queue = Queue()
+        workers = (initialize_main_workers(num_workers - 1, (queue, root_dir,
+                   yaml_config, config, num_frames, anlys, save_tracks,
+                   dets_min_score, vis_tracks, motchallenge, prog_queue)))
+        initialize_progress_worker(prog_queue, len(dirs))
+
+        for input_dir in detect.callback(
+                root_dir, yaml_config, vis_tracks, True, num_frames=num_frames,
+                motchallenge=motchallenge, iterate=True, multi_sequence=True,
+                always=always):
+            queue.put(input_dir)
+        queue.put(SENTINEL)
+
+        for worker in workers:
+            worker.join()
+        prog_queue.put(SENTINEL)
 
 
-def process(root_dir, yaml_config, tracker, num_frames, anlys,
-            det, save_tracks, dets_min_score, vis_tracks, motchallenge,
-            num_workers, input_dir):
-    prefix = f'(Image sequence: {input_dir})'
-    epic.LOGGER.info(f'{prefix} Processing.')
+def process(queue, root_dir, yaml_config, config, num_frames, anlys,
+            save_tracks, dets_min_score, vis_tracks, motchallenge,
+            prog_queue=None):
+    i = 0
+    while True:
+        if i != 0 and prog_queue is not None:
+            prog_queue.put(None)
+        i += 1
+        input_dir = queue.get()
+        if input_dir == SENTINEL:
+            break
+        prefix = f'(Image sequence: {input_dir})'
+        epic.LOGGER.info(f'{prefix} Processing.')
 
-    imgs = (load_imgs(input_dir) if not motchallenge else load_imgs(
-            os.path.join(input_dir, epic.OFFL_MOTC_IMGS_DIRNAME)))
-    if len(imgs) < 2:
-        epic.LOGGER.error(f'{prefix} Less than 2 images found, skipping...')
-        return
-    motc_dets_path = os.path.join(input_dir, epic.DETECTIONS_DIR_NAME, (
-        epic.MOTC_DETS_FILENAME)) if not motchallenge else (os.path.join(
-            input_dir, epic.OFFL_MOTC_DETS_DIRNAME,
-            epic.OFFL_MOTC_DETS_FILENAME))
+        imgs = (load_imgs(input_dir) if not motchallenge else load_imgs(
+                os.path.join(input_dir, epic.OFFL_MOTC_IMGS_DIRNAME)))
+        if len(imgs) < 2:
+            epic.LOGGER.error(f'{prefix} Less than 2 images found, '
+                              'skipping...')
+            continue
 
-    if det == 'always' or (not os.path.isfile(motc_dets_path) and (
-                           det == 'if-necessary')):
-        if num_workers != 1:
-            lock.acquire()
-        epic.LOGGER.info(f'{prefix} Detecting objects.')
-        detect.callback(
-            input_dir, yaml_config, vis_tracks, True, num_frames=num_frames,
-            motchallenge=motchallenge)
-        if num_workers != 1:
-            lock.release()
-    if not os.path.isfile(motc_dets_path):
-        epic.LOGGER.error(f'{prefix} Could not find or generate detections '
-                          'file, skipping...')
-        return
+        motc_dets_path = os.path.join(input_dir, epic.DETECTIONS_DIR_NAME, (
+            epic.MOTC_DETS_FILENAME)) if not motchallenge else (os.path.join(
+                input_dir, epic.OFFL_MOTC_DETS_DIRNAME,
+                epic.OFFL_MOTC_DETS_FILENAME))
 
-    dets = load_motc_dets(motc_dets_path, dets_min_score)
-    if len(dets) < 2:
-        epic.LOGGER.error(f'{prefix} Less than 2 frames with detections, '
-                          'skipping...')
-        return
+        if not os.path.isfile(motc_dets_path):
+            epic.LOGGER.error(f'{prefix} Could not find detections file, '
+                              'skipping...')
+            continue
 
-    if num_frames is None:
-        num_frames = min(len(imgs), len(dets))
-    elif len(imgs) < num_frames or len(dets) < num_frames:
-        epic.LOGGER.error(f'{prefix} Number of images and/or frames with '
-                          'detections is less than specified --num-frames, '
-                          'skipping...')
-        return
+        dets = load_motc_dets(motc_dets_path, dets_min_score)
+        if len(dets) < 2:
+            epic.LOGGER.error(f'{prefix} Less than 2 frames with detections, '
+                              'skipping...')
+            continue
 
-    imgs, dets = imgs[0: num_frames], dets[0: num_frames]
-    dets = create_tracklets(dets, imgs)
+        if num_frames is None:
+            num_frames = min(len(imgs), len(dets))
+        elif len(imgs) < num_frames or len(dets) < num_frames:
+            epic.LOGGER.error(f'{prefix} Number of images and/or frames with '
+                              'detections is less than specified'
+                              '--num-frames, skipping...')
+            continue
 
-    ldg_es = detect_leading_edges(imgs[0][1], dets[0])
-    if ldg_es is None:
-        epic.LOGGER.error(f'{prefix} Could not detect leading edges, '
-                          'skipping...')
-        return
+        imgs, dets = imgs[0: num_frames], dets[0: num_frames]
+        dets = create_tracklets(dets, imgs)
 
-    # epic.LOGGER.info(f'{prefix} Tracking objects.')
-    tracks = tracker.run(dets, ldg_es, imgs)
+        ldg_es = detect_leading_edges(imgs[0][1], dets[0])
+        if ldg_es is None:
+            epic.LOGGER.error(f'{prefix} Could not detect leading edges, '
+                              'skipping...')
+            continue
 
-    curr_output_dir = os.path.join(input_dir, TRACKS_DIR_NAME)
-    if os.path.isdir(curr_output_dir):
-        rmtree(curr_output_dir)
-    os.mkdir(curr_output_dir)
+        epic.LOGGER.info(f'{prefix} Tracking objects.')
+        tkr_fcty = TrackerFactory()
+        tracker = tkr_fcty.get_tracker(config['tracking']['tracker_name'],
+                                       config)
+        tracks = tracker.run(dets, ldg_es, imgs)
 
-    if save_tracks:
-        # epic.LOGGER.info(f'{prefix} Saving tracks..')
-        save_motc_tracks(tracks, MOTC_TRACKS_FILENAME, curr_output_dir)
-        if motchallenge:
-            tracks_dir = os.path.join(input_dir, epic.OFFL_MOTC_TRACKS_DIRNAME)
-            if not os.path.isdir(tracks_dir):
-                os.mkdir(tracks_dir)
-            save_motc_tracks(tracks, epic.OFFL_MOTC_TRACKS_FILENAME,
-                             tracks_dir)
-            motc_all_tracks_dir = (os.path.join(root_dir,
-                                   epic.OFFL_MOTC_ALL_TRACKS_DIRNAME))
-            if not os.path.isdir(motc_all_tracks_dir):
-                os.mkdir(motc_all_tracks_dir)
-            save_motc_tracks(tracks, f'{os.path.basename(input_dir)}'
-                             '.txt', motc_all_tracks_dir)
+        curr_output_dir = os.path.join(input_dir, TRACKS_DIR_NAME)
+        if os.path.isdir(curr_output_dir):
+            rmtree(curr_output_dir)
+        os.mkdir(curr_output_dir)
 
-    if vis_tracks:
-        # epic.LOGGER.info(f'{prefix} Visualizing tracks.')
-        draw_tracks(tracks, imgs)
-        save_imgs(imgs, curr_output_dir)
-        vid_path = os.path.join(curr_output_dir, VID_FILENAME)
-        save_video(imgs, vid_path)
+        if save_tracks:
+            epic.LOGGER.info(f'{prefix} Saving tracks.')
+            save_motc_tracks(tracks, MOTC_TRACKS_FILENAME, curr_output_dir)
+            if motchallenge:
+                tracks_dir = os.path.join(input_dir,
+                                          epic.OFFL_MOTC_TRACKS_DIRNAME)
+                if not os.path.isdir(tracks_dir):
+                    os.mkdir(tracks_dir)
+                save_motc_tracks(tracks, epic.OFFL_MOTC_TRACKS_FILENAME,
+                                 tracks_dir)
+                motc_all_tracks_dir = (os.path.join(root_dir,
+                                       epic.OFFL_MOTC_ALL_TRACKS_DIRNAME))
+                if not os.path.isdir(motc_all_tracks_dir):
+                    os.mkdir(motc_all_tracks_dir)
+                save_motc_tracks(tracks, f'{os.path.basename(input_dir)}'
+                                 '.txt', motc_all_tracks_dir)
 
-    if anlys:
-        analyse.callback(input_dir, yaml_config, num_workers=1)
+        if vis_tracks:
+            epic.LOGGER.info(f'{prefix} Visualizing tracks.')
+            draw_tracks(tracks, imgs)
+            save_imgs(imgs, curr_output_dir)
+            vid_path = os.path.join(curr_output_dir, VID_FILENAME)
+            save_video(imgs, vid_path)
 
-    # epic.LOGGER.info(f'{prefix} Finished processing.')
+        if anlys:
+            analyse.callback(input_dir, yaml_config, num_workers=1)
+
+        epic.LOGGER.info(f'{prefix} Finished processing.')
+
+    if prog_queue is not None:
+        queue.put(SENTINEL)
 
     return 0
 
 
-def init_lock(lk):
-    global lock
-    lock = lk
+def initialize_main_workers(num_workers, args):
+    workers = []
+    for i in range(0, num_workers):
+        worker = Process(target=process, args=args)
+        workers.append(worker)
+
+    for worker in workers:
+        worker.start()
+
+    return workers
+
+
+def initialize_progress_worker(prog_queue, total):
+    worker = Process(target=progress, args=(prog_queue, total))
+    worker.start()
+
+
+def progress(prog_queue, total):
+    with alive_bar(total) as main_bar:
+        i = 0
+        for item in iter(prog_queue.get, SENTINEL):
+            if item is None:
+                i += 1
+                main_bar()
+        main_bar(incr=total - i)
+
+        return 0
