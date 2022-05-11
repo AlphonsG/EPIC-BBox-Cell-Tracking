f36d26958be35fb1855f4a032f16e85584f1c413
Alphons <alphonsg@protonmail.com>
2022-05-12 00:32:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-import os
-import shutil
-from setuptools.command.develop import develop
 from setuptools import find_packages, setup
-from setuptools.command.install import install
-
-EPIC_HOME_DIRNAME = 'epic'
-EPIC_DIRS = ['misc/configs', 'misc/notebooks', 'misc/examples']
 
 REQUIRED_PACKAGES = [
     'moviepy',
@@ -13,60 +6,26 @@ REQUIRED_PACKAGES = [
     'seaborn',
     'natsort',
     'opencv-python',
-    'click',
     'imutils',
-    'lapsolver',
+    'lapsolver @ git+https://github.com/AlphonsG/py-lapsolver',
     'scikit-image',
-    'pywin32==225; platform_system=="Windows"',
     'alive-progress',
     'logger-tt',
-    'jupyter-client<7.0',
-    'importlib-metadata<4.0'
+    'openmim'
 ]
-
-
-def setup_epic_home_dir():
-    print('\nSetting up EPIC home directory')
-    epic_home_path = os.path.expanduser(os.path.join('~',
-                                        f'{EPIC_HOME_DIRNAME}'))
-    if os.path.isdir(epic_home_path):
-        shutil.rmtree(epic_home_path)
-    os.mkdir(epic_home_path)
-
-    curr_dir = os.path.abspath(os.path.dirname(__file__))
-    for epic_dir in EPIC_DIRS:
-        src = os.path.join(curr_dir, epic_dir)
-        dst = os.path.join(epic_home_path, os.path.basename(epic_dir))
-        shutil.copytree(src, dst)
-
-    print('Home directory successfully set up')
-
-
-class PostDevelop(develop):
-    """Pre-installation for development mode."""
-    def run(self):
-        develop.run(self)
-        setup_epic_home_dir()
-
-
-class PostInstall(install):
-    """Pre-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        setup_epic_home_dir()
 
 
 setup(
     name='epic',
     version='0.1.0',
     author='Alphons Gwatimba',
-    author_email='alphonsg@protonmail.com',
+    author_email='0go0vdp95@mozmail.com',
     packages=find_packages(),
-    url=('https://github.com/AlphonsGwatimba/Fast-AI-Enabled-Cell-Tracking-'
-         'and-Migration-Analysis-for-High-Throughput-Drug-Screening'),
+    url=('https://github.com/AlphonsG/EPIC-BBox-Cell-Tracking'),
     license='MIT',
-    description=('Fast AI-Enabled Cell Tracking and Migration Analysis for '
-                 'High-Throughput Drug Screening.'),
+    description=('AI-driven Cell Tracking to Enable High-Throughput Drug '
+                 'Screening Targeting Airway Epithelial Repair for Children '
+                 'with Asthma.'),
     long_description=open('README.md', encoding="utf-8").read(),
     install_requires=REQUIRED_PACKAGES,
     entry_points={
@@ -74,9 +33,5 @@ setup(
             'epic = epic.__main__:main'
         ]
     },
-    python_requires='>3.6',
-    cmdclass={
-        'develop': PostDevelop,
-        'install': PostInstall,
-    }
+    python_requires='>=3.8'
 )
