3deef2c3755b3a32021fbe6f0b6e2b4a1cf037e8
Alphons <alphonsg@protonmail.com>
2022-05-12 02:51:29 +0800

    Add ability for logger to bind to free port

--- epic/logging/logging.py
+++ epic/logging/logging.py
@@ -11,6 +11,13 @@ curr_dir = os.path.abspath(os.path.dirname(__file__))
 LOG_CONFIG_PATH = os.path.join(curr_dir, '..', '..', 'misc', 'configs',
                                'log_config.yaml')
 use_mp = False if os.environ.get('EPIC_LOGGING_NO_MP') == 'TRUE' else True
-setup_logging(use_multiprocessing=use_mp, config_path=LOG_CONFIG_PATH,
-              log_path='')
+
+for i in range(1024, 65535): 
+    try:
+        setup_logging(use_multiprocessing=use_mp, config_path=LOG_CONFIG_PATH,
+                      log_path='', port=i)
+    except OSError:
+        continue
+    break
+    
 LOGGER = logger
